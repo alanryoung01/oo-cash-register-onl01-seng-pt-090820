e6373d54747c13f6d502ffb396e2a1ea0fd7cc5d
Alan Young <alanryoung01@gmail.com>
2020-09-30 16:34:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
+  attr_accessor :total, :items, :discount, :last_trans
 
+  def initiaize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(name, price, amount = 1)
+    if amount > 1
+      i=0
+      while i < amount
+        @items << name
+        i+=1
+      end
+    else
+      @items << name
+    end
+    @total += price*amount
+    @last_trans = @total
+    @total
+  end
+
+  def apply_discount
+    if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - (@total * (@discount))
+
+    end
+  end
+
+end
