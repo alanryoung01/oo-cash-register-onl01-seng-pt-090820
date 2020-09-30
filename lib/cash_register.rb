082316d9798ea408abc3bac7d666167a55d3b872
Alan Young <alanryoung01@gmail.com>
2020-09-30 18:21:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,43 +1,39 @@
-require 'pry'
 class CashRegister
-
-  attr_accessor :total, :discount, :last_trans, :items
+  attr_accessor :total, :discount, :quantity, :price
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @price = price
+    @quantity = quantity
     @items = []
+    @transactions = []
   end
 
-
-  def add_item(name,price,items = 1)
-    if items>1
-      i=0
-      while i<items
-        @items << name
-        i+=1
-      end
-    else
-      @items << name
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    @transactions << price
+    i = quantity
+    until i == 0 do
+      @items << title
+      i -= 1
     end
-    @total += price*items
-    @last_trans = @total
-    @total
   end
 
-  def apply_discount()
-    if @discount > 0
-      @discount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
-      "After the discount, the total comes to $#{@total.to_i}."
-    else
+  def apply_discount
+    if @discount == 0
       "There is no discount to apply."
+    else
+      self.total -= (0.01 * @discount * @total).to_i
+      "After the discount, the total comes to $#{self.total}."
     end
   end
 
-
-  def void_last_transaction()
-    @total -= @last_trans
+  def items
+    @items
   end
 
+  def void_last_transaction
+    self.total = @total - @transactions.pop
+  end
 end
